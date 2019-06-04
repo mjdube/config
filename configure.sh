c9b41bb1a0dfedaec420a330b0ced905899ef00e
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-06-04 16:33:27 +0200

    save point

--- configure.sh
+++ configure.sh
@@ -1,6 +1,16 @@
 #! bin/bash
 
-git clone https://github.com/tcajee/configs.git ~/.configs
+echo "installing Oh-My-ZSH"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Done."
 cd ~/.configs
+echo "installing Vimrc"
 cp -R .vim_runtime ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+sh ~/.vim_runtime/install_vimrc.sh
+echo "Done."
+echo "Copying .zshrc"
+cp .zshrc ..
+echo "Done."
+echo "Copying .bashrc"
+cp .bashrc ..
+echo "Done."
